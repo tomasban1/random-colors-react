f14b7b507cd5258659dc106103aa22fac2fc688b
tomasban1 <tomasbanius@gmail.com>
2024-09-14 20:14:19 +0300

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+
+import { useState } from 'react'
+import './App.css'
+import { ButtonContainer } from './components/ButtonContainer'
+
+function App() {
+  const [color, setColor] = useState('#000000');
+  const [color2, setColor2] = useState('#000000');
+  const [color3, setColor3] = useState('#000000');
+  const [color4, setColor4] = useState('#000000');
+  const [backgrounColor, setBackGroundColor] = useState('#b48e3d');
+  
+  const btnNames = [
+    {
+      name1: 'Click me',
+      name2: 'And me',
+      name3: 'Me too',
+      name4: 'Me also',
+    },
+  ];
+
+  function randomColor(){
+    const random = Math.floor(Math.random() * 10000000).toString(16);
+    const randomBackground = Math.floor(Math.random() * 15158899).toString(16);
+    setColor('#' + random)
+    setBackGroundColor('#' + randomBackground)
+   
+    
+  }
+  function randomColor2(){
+    const random = Math.floor(Math.random() * 10000000).toString(16);
+    const randomBackground = Math.floor(Math.random() * 15158899).toString(16);
+    setColor2('#' + random)
+    setBackGroundColor('#' + randomBackground)
+    
+  }
+  function randomColor3(){
+  const random = Math.floor(Math.random() * 10000000).toString(16);
+    const randomBackground = Math.floor(Math.random() * 15158899).toString(16);
+    setColor3('#' + random)
+    setBackGroundColor('#' + randomBackground)
+    
+  }
+  function randomColor4(){
+    const random = Math.floor(Math.random() * 10000000).toString(16);
+    const randomBackground = Math.floor(Math.random() * 15158899).toString(16);
+    setColor4('#' + random)
+    setBackGroundColor('#' + randomBackground)
+    
+  }
+  
+
+  return (
+    <>
+      <div className='mainContainer' style={{backgroundColor:backgrounColor}}>
+        <h1>Random colors</h1>
+        <ButtonContainer 
+        btnNames = {btnNames} 
+        generateColor1 = {randomColor} 
+        generateColor2 = {randomColor2} 
+        generateColor3 = {randomColor3} 
+        generateColor4 = {randomColor4} 
+        currColor1 = {color}
+        currColor2 = {color2}
+        currColor3 = {color3}
+        currColor4 = {color4}
+        background = {backgrounColor}
+         />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ButtonContainer.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+
+
+export function ButtonContainer(props){
+    const {
+        btnNames, 
+        generateColor1, 
+        generateColor2, 
+        generateColor3, 
+        generateColor4, 
+        currColor1, 
+        currColor2, 
+        currColor3, 
+        currColor4, 
+        background} = props
+
+    return (
+        <>
+            <div className="bd-example m-0 border-0 gap-3 d-flex">  
+                {btnNames.map((name, index) => 
+                <>
+                    <div className="d-flex flex-column">
+                        <button onClick={generateColor1} key={index} type="button" className="btn btn-primary" style={{backgroundColor:currColor1}}>{name.name1}</button>
+                        <span style={{width: 81}}>Color code: {currColor1}</span>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <button onClick={generateColor2} type="button" key={index} className="btn btn-secondary" style={{backgroundColor:currColor2}}>{name.name2}</button>
+                        <span style={{width: 81}}>Color code: {currColor2}</span>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <button onClick={generateColor3} type="button" key={index} className="btn btn-success" style={{backgroundColor:currColor3}}>{name.name3}</button>
+                        <span style={{width: 81}}>Color code: {currColor3}</span>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <button onClick={generateColor4} type="button" key={index} className="btn btn-danger" style={{backgroundColor:currColor4}}>{name.name4}</button>
+                        <span style={{width: 81}}>Color code: {currColor4}</span>
+                    </div>
+                </>
+                )}
+            </div>
+            <div style={{marginTop: 50, width:300}}>
+                <h2>Screen background color code: {background}</h2>
+            </div>
+        </>
+        
+    );
+}
